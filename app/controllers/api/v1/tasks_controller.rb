9d4c57e93c53c3833a2a219dcfc63730aa887185
ashly <zhaoyuting@china-revival.com>
2026-02-25 08:34:52 -0800

    1

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -5,22 +5,22 @@ module Api
 
 			def fetch_next_executable_task
 				loop do
-					# candidate_id = MoveTask.with_account_unused_today.limit(1).pluck(:id).first
-					# return render json: {id: nil,video_url: nil,social_account_id: nil,adspower_user_name: nil,account_type: nil,title: nil} unless candidate_id
+					candidate_id = MoveTask.with_account_unused_today.limit(1).pluck(:id).first
+					return render json: {id: nil,video_url: nil,social_account_id: nil,adspower_user_name: nil,account_type: nil,title: nil} unless candidate_id
 
-					# today_begin = Time.current.beginning_of_day
-    	# 			used_account_ids = MoveTask.where(start_at: today_begin..).distinct.pluck(:account_id).compact
-    	# 			if used_account_ids.empty?
-					# 	affected = MoveTask.where(id: candidate_id, status: :waiting_publish).update_all(status: :executing,start_at: Time.current,updated_at: Time.current)
-					# else
-					# 	affected = MoveTask.where(id: candidate_id, status: :waiting_publish).where.not(account_id: used_account_ids).update_all(status: :executing,start_at: Time.current,updated_at: Time.current)
-					# end
+					today_begin = Time.current.beginning_of_day
+    				used_account_ids = MoveTask.where(start_at: today_begin..).distinct.pluck(:account_id).compact
+    				if used_account_ids.empty?
+						affected = MoveTask.where(id: candidate_id, status: :waiting_publish).update_all(status: :executing,start_at: Time.current,updated_at: Time.current)
+					else
+						affected = MoveTask.where(id: candidate_id, status: :waiting_publish).where.not(account_id: used_account_ids).update_all(status: :executing,start_at: Time.current,updated_at: Time.current)
+					end
 
-					# next if affected.zero?
+					next if affected.zero?
 					
-					# task = MoveTask.find(candidate_id)
+					task = MoveTask.find(candidate_id)
 
-					task = MoveTask.find(2)
+					# task = MoveTask.find(2)
 					return render json: {id: task.id,video_url: task.video_url,social_account_id: task.source_account_url,adspower_user_name: task.browser.profile_name,account_type: task.platform,title: task.title}
 				end
 			end
