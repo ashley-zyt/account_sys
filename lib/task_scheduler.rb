aba481267fc78243df7102b7954a1634c3b1f681
ashly <zhaoyuting@china-revival.com>
2026-02-27 07:51:32 -0800

    1

--- lib/task_scheduler.rb
+++ lib/task_scheduler.rb
@@ -8,7 +8,7 @@ class TaskScheduler
 		# 		TaskAllocator.allocate(task)
 		# 	end
 		# end
-		Account.active.each do |account|
+		Account.active.where(work_type:0).each do |account|
 			task = MoveTask.where(status:"pending").where(platform:account.platform).order("created_at asc").first
 			if !task.nil?
 				task.update(account_id: account.id,browser_id: account.browser_id,status:waiting_publish)
