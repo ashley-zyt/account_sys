bf961b523b0c0151c31441668efdf8f21d6931aa
ashly <zhaoyuting@china-revival.com>
2026-02-27 07:32:06 -0800

    1

--- app/controllers/admin/move_tasks_controller.rb
+++ app/controllers/admin/move_tasks_controller.rb
@@ -1,7 +1,7 @@
 class Admin::MoveTasksController < Admin::BaseController
 	def index
 		@q = MoveTask.ransack(params[:q])
-		@move_tasks = @q.result(distinct: true).includes(:account, :browser).order(actual_publish_time: :desc).page(params[:page]).per(10)
+		@move_tasks = @q.result(distinct: true).includes(:account, :browser).order(updated_at: :desc).page(params[:page]).per(10)
 	end
 
 	def show
