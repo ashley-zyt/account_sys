d3a251cfe7a81243a8b49986f434cd4993f00754
ashly <zhaoyuting@china-revival.com>
2026-02-28 23:21:39 -0800

    1

--- lib/task_scheduler.rb
+++ lib/task_scheduler.rb
@@ -1,17 +1,15 @@
 class TaskScheduler
 	def self.pending_task
-		# 获取所有有 pending 任务的平台（去重）
-		# platforms_with_pending = MoveTask.where(status: :pending).distinct.pluck(:platform)
-
-		# platforms_with_pending.each do |platform|
-		# 	MoveTask.pending_for_platform(platform).limit(20).each do |task|
-		# 		TaskAllocator.allocate(task)
-		# 	end
-		# end
 		Account.active.where(work_type:0).each do |account|
 			task = MoveTask.where(status:"pending").where(platform:account.platform).order("created_at asc").first
 			if !task.nil?
-				task.update(account_id: account.id,browser_id: account.browser_id,status:waiting_publish)
+				task.update(account_id: account.id,browser_id: account.browser_id,status:"waiting_publish")
+			end
+		end
+		Account.active.where(browser_id:[1, 2, 3, 4, 5, 6, 7, 8]).each do |account|
+			task = MoveTask.where(status:"pending",work_type:0).where(platform:account.platform).order("created_at asc").first
+			if !task.nil?
+				task.update(account_id: account.id,browser_id: account.browser_id,status:"waiting_publish")
 			end
 		end
 	end
