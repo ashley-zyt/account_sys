16997da5ef9dbcf85724d092ac14d4d5fd244a92
ashly <zhaoyuting@china-revival.com>
2026-02-28 22:11:11 -0800

    1

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -4,25 +4,30 @@ module Api
 			skip_before_action :verify_authenticity_token, only: [:report]
 
 			def fetch_next_executable_task
-				loop do
-					candidate_id = MoveTask.with_account_unused_today.limit(1).pluck(:id).first
-					return render json: {id: nil,video_url: nil,social_account_id: nil,adspower_user_name: nil,account_type: nil,title: nil} unless candidate_id
-
-					today_begin = Time.current.beginning_of_day
-    				used_account_ids = MoveTask.where(start_at: today_begin..).distinct.pluck(:account_id).compact
-    				if used_account_ids.empty?
-						affected = MoveTask.where(id: candidate_id, status: :waiting_publish).update_all(status: :executing,start_at: Time.current,updated_at: Time.current)
-					else
-						affected = MoveTask.where(id: candidate_id, status: :waiting_publish).where.not(account_id: used_account_ids).update_all(status: :executing,start_at: Time.current,updated_at: Time.current)
-					end
-
-					next if affected.zero?
-					Rails.logger.info candidate_id
-					task = MoveTask.find(candidate_id)
-					Rails.logger.info JSON.parse(task.to_json)
-					# task = MoveTask.find(120)
-					return render json: {id: task.id,video_url: task.video_url,social_account_id: task.source_account_url,adspower_user_name: task.browser.profile_name,account_type: task.platform,title: task.title}
+				task = MoveTask.where(status:"waiting_publish").limit(1)
+				account = task.account
+				if !account.nil? && account.status == "正常"
+					task.update(status:"executing")
 				end
+				return render json: {id: task.id,video_url: task.video_url,social_account_id: task.source_account_url,adspower_user_name: task.browser.profile_name,account_type: task.platform,title: task.title}
+				# loop do
+				# 	candidate_id = MoveTask.with_account_unused_today.limit(1).pluck(:id).first
+				# 	return render json: {id: nil,video_url: nil,social_account_id: nil,adspower_user_name: nil,account_type: nil,title: nil} unless candidate_id
+
+				# 	today_begin = Time.current.beginning_of_day
+    # 				used_account_ids = MoveTask.where(start_at: today_begin..).distinct.pluck(:account_id).compact
+    # 				if used_account_ids.empty?
+				# 		affected = MoveTask.where(id: candidate_id, status: :waiting_publish).update_all(status: :executing,start_at: Time.current,updated_at: Time.current)
+				# 	else
+				# 		affected = MoveTask.where(id: candidate_id, status: :waiting_publish).where.not(account_id: used_account_ids).update_all(status: :executing,start_at: Time.current,updated_at: Time.current)
+				# 	end
+
+				# 	next if affected.zero?
+				# 	Rails.logger.info candidate_id
+				# 	task = MoveTask.find(candidate_id)
+				# 	Rails.logger.info JSON.parse(task.to_json)
+				# 	return render json: {id: task.id,video_url: task.video_url,social_account_id: task.source_account_url,adspower_user_name: task.browser.profile_name,account_type: task.platform,title: task.title}
+				# end
 			end
 			def report
 				task_id = params[:id].to_s.strip
