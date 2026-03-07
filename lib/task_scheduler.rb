fdfc857d7594c2bef2d450f36f55b35f1730c16b
ashly <zhaoyuting@china-revival.com>
2026-03-07 03:59:32 -0800

    1

--- lib/task_scheduler.rb
+++ lib/task_scheduler.rb
@@ -1,16 +1,16 @@
 class TaskScheduler
 	def self.pending_task
-		# Account.active.where(work_type:0).each do |account|
+		Account.active.where(work_type:0).each do |account|
+			task = MoveTask.where(status:"pending").where(platform:account.platform,theme:account["theme"]).order("created_at asc").first
+			if !task.nil?
+				task.update(account_id: account.id,browser_id: account.browser_id,status:"waiting_publish")
+			end
+		end
+		# Account.active.where(browser_id:[1, 2, 3, 4, 5, 6, 7, 8]).each do |account|
 		# 	task = MoveTask.where(status:"pending").where(platform:account.platform).order("created_at asc").first
 		# 	if !task.nil?
 		# 		task.update(account_id: account.id,browser_id: account.browser_id,status:"waiting_publish")
 		# 	end
 		# end
-		Account.active.where(browser_id:[1, 2, 3, 4, 5, 6, 7, 8]).each do |account|
-			task = MoveTask.where(status:"pending").where(platform:account.platform).order("created_at asc").first
-			if !task.nil?
-				task.update(account_id: account.id,browser_id: account.browser_id,status:"waiting_publish")
-			end
-		end
 	end
 end
