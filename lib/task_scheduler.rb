4b6cef0ba649b65b5cdb2f41a11ed939a86972d5
ashly <zhaoyuting@china-revival.com>
2026-03-31 04:19:12 -0700

    1

--- lib/task_scheduler.rb
+++ lib/task_scheduler.rb
@@ -1,5 +1,13 @@
 class TaskScheduler
 	def self.pending_task
+		# 释放亨亨猫类型的错误资源
+		TaskLog.where(status: "failed", error_msg: "哼哼猫下载视频异常").each do |task_log|
+			task = MoveTask.find_by(task_uuid:task_log.task_uuid)
+			if task.error_msg == "哼哼猫下载视频异常"
+				task.update(status:"pending",error_msg:nil,start_at:nil,actual_publish_time:nil,browser_id:nil)
+			end
+		end
+		# 分配今日资源
 		Account.active.where(work_type:0).each do |account|
 			task = MoveTask.where(status:"pending").where(platform:account.platform,theme:account["theme"]).order("created_at asc").first
 			if !task.nil?
