48a9cdf20753b1d0085d86f4db6123da3123422d
ashly <zhaoyuting@china-revival.com>
2026-04-04 22:13:35 -0700

    1

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -6,19 +6,19 @@ module Api
 			def fetch_next_executable_task
 				next_task = MoveTask.where(status:"waiting_publish").where("account_id is not null").first
 				# 释放亨亨猫类型的错误资源
-				TaskLog.where(status: "failed").where("error_msg like '%哼哼猫%' or error_msg like '%亨亨猫%'").where("created_at > ?",Time.now-1.days).each do |task_log|
-					task = MoveTask.find_by(task_uuid:task_log.task_uuid)
-					if task.error_msg.include?"哼哼猫" or task.error_msg.include?"亨亨猫"
-						task.update(status:"pending",error_msg:nil,start_at:nil,actual_publish_time:nil,browser_id:nil)
-					end
-				end
-				# 分配今日资源
-				Account.active.where(work_type:0).each do |account|
-					task = MoveTask.where(status:"pending").where(platform:account.platform,theme:account["theme"]).order("created_at asc").first
-					if !task.nil?
-						task.update(account_id: account.id,browser_id: account.browser_id,status:"waiting_publish")
-					end
-				end
+				# TaskLog.where(status: "failed").where("error_msg like '%哼哼猫%' or error_msg like '%亨亨猫%'").where("created_at > ?",Time.now-1.days).each do |task_log|
+				# 	task = MoveTask.find_by(task_uuid:task_log.task_uuid)
+				# 	if task.error_msg.include?"哼哼猫" or task.error_msg.include?"亨亨猫"
+				# 		task.update(status:"pending",error_msg:nil,start_at:nil,actual_publish_time:nil,browser_id:nil)
+				# 	end
+				# end
+				# # 分配今日资源
+				# Account.active.where(work_type:0).each do |account|
+				# 	task = MoveTask.where(status:"pending").where(platform:account.platform,theme:account["theme"]).order("created_at asc").first
+				# 	if !task.nil?
+				# 		task.update(account_id: account.id,browser_id: account.browser_id,status:"waiting_publish")
+				# 	end
+				# end
 				return render json: {id: nil,video_url: nil,social_account_id: nil,adspower_user_name: nil,account_type: nil,title: nil} if next_task.nil?
 				id = next_task.id
 				next_task.update(status:"executing")
