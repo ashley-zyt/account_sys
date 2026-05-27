52f26cae175f21d41ab78df98cf0c7597d4192bb
ashly <zhaoyuting@china-revival.com>
2026-05-27 16:00:52 +0800

    1

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: themes
+#
+#  id            :bigint           not null, primary key
+#  name          :string(255)      not null
+#  oss_directory :string(255)
+#  remark        :text(65535)
+#  titles        :text(65535)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_themes_on_name           (name) UNIQUE
+#  index_themes_on_oss_directory  (oss_directory) UNIQUE
+#
 class Theme < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
