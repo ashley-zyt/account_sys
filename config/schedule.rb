e4b45fbf93d1e109eda465807c65451e439015d7
ashly <zhaoyuting@china-revival.com>
2026-05-29 18:19:35 +0800

    1

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,3 +4,7 @@ set :output, "log/pending_task.log"
 every :day, at: '15:50' do
   runner 'TaskScheduler.pending_task'
 end
+
+every :day, at: '09:00' do
+  runner 'TaskScheduler.assign_operation_resources'
+end
--- lib/task_scheduler.rb
+++ lib/task_scheduler.rb
@@ -15,4 +15,35 @@ class TaskScheduler
 			end
 		end
 	end
+
+	def self.assign_operation_resources
+		today = Date.today
+		today_start = today.beginning_of_day
+		today_end = today.end_of_day
+
+		Account.active.where(work_type: "人工运营").each do |account|
+			has_posted_today = OperationTask.exists?(
+				account_id: account.id,
+				status: :completed,
+				actual_publish_time: today_start..today_end
+			)
+
+			next if has_posted_today
+
+			pending_task = OperationTask.where(status: :pending, platform: account.platform).order(created_at: :asc).first
+
+			if pending_task
+				ActiveRecord::Base.transaction do
+					pending_task.update!(
+						account_id: account.id,
+						browser_id: account.browser_id,
+						status: :processing
+					)
+				end
+				Rails.logger.info "人工运营账号 #{account.account_name}[#{account.platform}] 分配运营资源成功"
+			else
+				Rails.logger.warn "人工运营账号 #{account.account_name}[#{account.platform}] 暂无可用运营资源"
+			end
+		end
+	end
 end
