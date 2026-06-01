1365432c968769b1dbbbcec477f67b8b59f7bb22
ashly <zhaoyuting@china-revival.com>
2026-06-01 20:16:18 +0800

    1

--- db/migrate/20260601200722_remove_platform_string_form_operation_tasks.rb
+++ db/migrate/20260601200722_remove_platform_string_form_operation_tasks.rb
@@ -1,6 +1,6 @@
 class RemovePlatformStringFormOperationTasks < ActiveRecord::Migration[6.1]
   def change
-    remove_index :operation_tasks, :index_operation_tasks_on_oss_url_and_platform_string
+    remove_index :operation_tasks, column: [:oss_url, :platform_string]
     remove_column :operation_tasks, :platform_string
     add_index :operation_tasks, [:oss_url, :platform], unique: true, name: 'index_operation_tasks_on_oss_url_and_platform'
   end
