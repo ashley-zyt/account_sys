1cadd9de8fe01686ee6acbae06f9fcc742dea256
ashly <zhaoyuting@china-revival.com>
2026-06-01 20:00:22 +0800

    1

--- db/migrate/20260601195601_change_operation_task_platform_to_integer.rb
+++ db/migrate/20260601195601_change_operation_task_platform_to_integer.rb
@@ -1,6 +1,9 @@
-class ChangeOperationTaskPlatformToInteger < ActiveRecord::Migration[6.1]
+class ChangeOperationTaskPlatformToInteger < ActiveRecord::Migration[7.0]
   def up
-    # 先备份数据
+    # 先删除唯一索引
+    remove_index :operation_tasks, name: 'index_operation_tasks_on_oss_url_and_platform' rescue nil
+    
+    # 备份数据
     rename_column :operation_tasks, :platform, :platform_string
     
     # 添加新的 integer 字段
@@ -8,22 +11,28 @@ class ChangeOperationTaskPlatformToInteger < ActiveRecord::Migration[6.1]
     
     # 迁移数据
     OperationTask.reset_column_information
+    platform_map = {
+      'facebook' => 1,
+      'twitter' => 2,
+      'tiktok' => 3,
+      'youtube' => 4,
+      'instagram' => 5
+    }
     OperationTask.find_each do |task|
-      platform_map = {
-        '1' => 1,
-        '2' => 2,
-        '3' => 3,
-        '4' => 4,
-        '5' => 5
-      }
       task.update_column(:platform, platform_map[task.platform_string] || 0)
     end
     
     # 删除旧字段
     remove_column :operation_tasks, :platform_string
+    
+    # 重建唯一索引
+    add_index :operation_tasks, [:oss_url, :platform], unique: true
   end
 
   def down
+    # 先删除唯一索引
+    remove_index :operation_tasks, name: 'index_operation_tasks_on_oss_url_and_platform' rescue nil
+    
     # 添加回 string 字段
     add_column :operation_tasks, :platform_string, :string
     
@@ -45,5 +54,8 @@ class ChangeOperationTaskPlatformToInteger < ActiveRecord::Migration[6.1]
     
     # 恢复原名字段
     rename_column :operation_tasks, :platform_string, :platform
+    
+    # 重建唯一索引
+    add_index :operation_tasks, [:oss_url, :platform], unique: true
   end
 end
