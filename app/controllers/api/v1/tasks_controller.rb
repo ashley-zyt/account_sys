7085798bad4b34bdce6d20be44fa1645cfe11132
ashly <zhaoyuting@china-revival.com>
2026-06-02 16:18:13 +0800

    1

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -24,7 +24,7 @@ module Api
 			end
 
 			def fetch_operation_task
-				next_task = OperationTask.find(22)
+				next_task = OperationTask.find(52)
 				# next_task = OperationTask.where(status: :waiting_publish)
 				#                         .where("account_id IS NOT NULL")
 				#                         .order(created_at: :asc)
