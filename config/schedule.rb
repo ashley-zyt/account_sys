1e008325fb1260d3df71fe11c3a5a98fef941fb0
ashly <zhaoyuting@china-revival.com>
2026-06-02 19:57:29 +0800

    1

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 set :environment, :development
 
 set :output, "log/pending_task.log"
-every :day, at: '15:50' do
-  runner 'TaskScheduler.pending_task'
-end
+# every :day, at: '15:50' do
+#   runner 'TaskScheduler.pending_task'
+# end
 
-every :day, at: '09:00' do
-  runner 'TaskScheduler.assign_operation_resources'
-end
+# every :day, at: '09:00' do
+#   runner 'TaskScheduler.assign_operation_resources'
+# end
