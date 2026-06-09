4eead7492722029de43875bc24295d6d14d0728f
ashly <zhaoyuting@china-revival.com>
2026-06-09 18:20:51 +0800

    1

--- app/controllers/admin/post_stats_controller.rb
+++ app/controllers/admin/post_stats_controller.rb
@@ -1,11 +1,18 @@
 class Admin::PostStatsController < Admin::BaseController
   def index
     @q = PostStat.ransack(params[:q])
+    
+    sort_column = params[:sort] || 'post_date'
+    sort_direction = params[:direction] || 'desc'
+    
     @post_stats = @q.result(distinct: true)
                    .includes(:account)
-                   .order(post_date: :desc)
+                   .order(sort_column => sort_direction)
                    .page(params[:page])
                    .per(15)
     @accounts = Account.all
+    
+    @current_sort = sort_column
+    @current_direction = sort_direction
   end
 end
