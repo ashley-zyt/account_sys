3a1d21a57e5b4dfbb09ba65c82c4c7367bd19792
ashly <zhaoyuting@china-revival.com>
2026-06-10 22:55:12 +0800

    1

--- app/controllers/admin/post_stats_controller.rb
+++ app/controllers/admin/post_stats_controller.rb
@@ -11,6 +11,8 @@ class Admin::PostStatsController < Admin::BaseController
                    .page(params[:page])
                    .per(15)
     @accounts = Account.all
+    @work_types = Account.work_types.invert  # 获取工作模式选项
+    @platforms = Account.platforms.invert    # 获取平台选项
     
     @current_sort = sort_column
     @current_direction = sort_direction
