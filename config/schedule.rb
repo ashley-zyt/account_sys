23ec815a42aeb3c7805622644b53f3ebc8ff9745
ashly <zhaoyuting@china-revival.com>
2026-07-01 14:44:33 +0800

    1

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,11 @@ set :output, "log/pending_task.log"
 #   runner 'TaskScheduler.pending_task'
 # end
 
+# 每日推送浏览器账号数据到Windows机器采集发文数据
+every :day, at: '03:00' do
+  runner 'PostDatas.fetch'
+end
+
 every :day, at: '11:50' do
   runner 'TaskScheduler.assign_operation_resources'
 end
---
+++ lib/post_datas.rb
@@ -0,0 +1,74 @@
+# 抓取所有发文的详细数据
+# 每日定期获取绑定正常账号的浏览器列表，推送到外部接口采集发文数据
+class PostDatas
+
+  def self.fetch
+    browsers = Browser
+                 .joins(:accounts)
+                 .where(accounts: { status: Account.statuses['正常'] })
+                 .where.not(accounts: { platform: Account.platforms['facebook'] })
+                 .distinct
+                 .order(created_at: :desc)
+
+    data = browsers.map do |browser|
+      active_accounts = browser.accounts
+                          .where(status: Account.statuses['正常'])
+                          .where.not(platform: Account.platforms['facebook'])
+      {
+        id: browser.id,
+        profile_name: browser.profile_name,
+        active_accounts: active_accounts.map do |acc|
+          {
+            id: acc.id,
+            platform: acc.platform,
+            source_url: acc.source_url
+          }
+        end
+      }
+    end
+
+    Rails.logger.info "[PostDatas] 共 #{data.size} 个浏览器需要采集发文数据"
+
+    success_count = 0
+    fail_count = 0
+
+    data.each do |browser_data|
+      response = push_to_external(browser_data)
+      if response[:success]
+        success_count += 1
+        Rails.logger.info "[PostDatas] 浏览器 #{browser_data[:profile_name]} 推送成功"
+      else
+        fail_count += 1
+        Rails.logger.error "[PostDatas] 浏览器 #{browser_data[:profile_name]} 推送失败: #{response[:error]}"
+      end
+    end
+
+    Rails.logger.info "[PostDatas] 采集完成: 成功 #{success_count} 个, 失败 #{fail_count} 个"
+    { success_count: success_count, fail_count: fail_count, total: data.size }
+  rescue => e
+    Rails.logger.error "[PostDatas] 执行异常: #{e.message}"
+    { success_count: 0, fail_count: 0, total: 0, error: e.message }
+  end
+
+  # 推送单个浏览器数据到外部接口
+  def self.push_to_external(browser_data)
+    uri = URI.parse("http://174.139.46.117:8080/accounts/fetch_posts")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.read_timeout = 300
+    http.open_timeout = 300
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request['Content-Type'] = 'application/json'
+    request.body = browser_data.to_json
+
+    response = http.request(request)
+
+    if response.code == '200'
+      { success: true, response: response.body }
+    else
+      { success: false, error: "HTTP #{response.code}: #{response.body}" }
+    end
+  rescue => e
+    { success: false, error: e.message }
+  end
+end
