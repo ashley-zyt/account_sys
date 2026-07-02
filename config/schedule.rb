984a34c5d3e23e00f213e68422135b990c371ff2
ashly <zhaoyuting@china-revival.com>
2026-07-02 16:35:01 +0800

    1

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,20 +27,22 @@ every :day, at: '12:00' do
 end
 
 # 分配资源 人工运营和Grok两种
-set :output, "log/taskscheduler_assignoperationresources.log"
-every :day, at: '16:50' do
-  runner 'TaskScheduler.assign_operation_resources'
-end
-set :output, "log/taskscheduler_assigngrokresources.log"
-every :day, at: '16:51' do
-  runner 'TaskScheduler.assign_grok_resources'
-end
+# set :output, "log/taskscheduler_assignoperationresources.log"
+# every :day, at: '16:50' do
+#   runner 'TaskScheduler.assign_operation_resources'
+# end
+# set :output, "log/taskscheduler_assigngrokresources.log"
+# every :day, at: '16:51' do
+#   runner 'TaskScheduler.assign_grok_resources'
+# end
 
-# 每日下午五点开始重试中午发布错误的人工运营和Grok资源
-set :output, "log/publishscheduler_run.log"
-every :day, at: '17:00' do
-  runner 'PublishScheduler.run'
-end
+# # 每日下午五点开始重试中午发布错误的人工运营和Grok资源
+# set :output, "log/publishscheduler_run.log"
+# every :day, at: '17:00' do
+#   runner 'PublishScheduler.run'
+# end
+
+
 # set :output, "log/check_timeout_tasks.log"
 # every 5.minutes do
 #   runner 'TaskScheduler.check_timeout_tasks'
