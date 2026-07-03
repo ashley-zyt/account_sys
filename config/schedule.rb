cf1f7f750f1eb4d59bac2ca832be310242fae38a
ashly <zhaoyuting@china-revival.com>
2026-07-03 22:14:58 +0800

    1

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,20 +27,20 @@ every :day, at: '12:00' do
 end
 
 # 分配资源 人工运营和Grok两种
-# set :output, "log/taskscheduler_assignoperationresources.log"
-# every :day, at: '16:50' do
-#   runner 'TaskScheduler.assign_operation_resources'
-# end
-# set :output, "log/taskscheduler_assigngrokresources.log"
-# every :day, at: '16:51' do
-#   runner 'TaskScheduler.assign_grok_resources'
-# end
+set :output, "log/taskscheduler_assignoperationresources.log"
+every :day, at: '16:50' do
+  runner 'TaskScheduler.assign_operation_resources'
+end
+set :output, "log/taskscheduler_assigngrokresources.log"
+every :day, at: '16:51' do
+  runner 'TaskScheduler.assign_grok_resources'
+end
 
-# # 每日下午五点开始重试中午发布错误的人工运营和Grok资源
-# set :output, "log/publishscheduler_run.log"
-# every :day, at: '17:00' do
-#   runner 'PublishScheduler.run'
-# end
+# 每日下午五点开始重试中午发布错误的人工运营和Grok资源
+set :output, "log/publishscheduler_run.log"
+every :day, at: '17:00' do
+  runner 'PublishScheduler.run'
+end
 
 # RedNote 关键词任务状态同步（每30分钟）
 set :output, "log/red_note_sync.log"
