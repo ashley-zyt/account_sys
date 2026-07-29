2f5323f12973d4e468a34e8329afbe19bf7b0742
ashly <zhaoyuting@china-revival.com>
2026-07-29 10:57:16 +0800

    1

--- db/migrate/20260729000001_add_source_filename_to_operation_tasks.rb
+++ db/migrate/20260729000001_add_source_filename_to_operation_tasks.rb
@@ -1,4 +1,4 @@
-class AddSourceFilenameToOperationTasks < ActiveRecord::Migration[7.0]
+class AddSourceFilenameToOperationTasks < ActiveRecord::Migration[6.1]
   def change
     add_column :operation_tasks, :source_filename, :string
     add_index :operation_tasks, :source_filename
