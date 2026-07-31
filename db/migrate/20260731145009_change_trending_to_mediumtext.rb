254cc92cc9e2e349a29af899e7b1089ce01a7cb8
ashly <zhaoyuting@china-revival.com>
2026-07-31 15:00:38 +0800

    1

--- db/migrate/20260731145009_change_trending_to_mediumtext.rb
+++ db/migrate/20260731145009_change_trending_to_mediumtext.rb
@@ -1,6 +1,6 @@
-class ChangeTrendingToMediumtext < ActiveRecord::Migration[6.0+]
-  def up
+class ChangeTrendingToMediumtext < ActiveRecord::Migration[6.1]
+  def change
     # 直接用 SQL 修改
-    execute "ALTER TABLE crypto_videos MODIFY trending MEDIUMTEXT"
+    change_column :crypto_videos, :trending, :text, limit: 16_777_215
   end
 end
