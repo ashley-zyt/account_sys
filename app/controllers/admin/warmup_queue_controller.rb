c920984b2d6a9f52d6bcc1d89886586d4c6e00a1
ashly <zhaoyuting@china-revival.com>
2026-08-05 17:55:03 +0800

    1

--- app/controllers/admin/warmup_queue_controller.rb
+++ app/controllers/admin/warmup_queue_controller.rb
@@ -30,11 +30,11 @@ class Admin::WarmupQueueController < Admin::BaseController
                             .where(browsers: { machine_ip: [nil, ""] })
                             .count
 
-    # 总计
-    @total_count = Account.where(status: [0, 3]).count
-    @enabled_count = WarmupProfile.where(warmup_enabled: true).count
-    @never_warmed = Account.joins(:warmup_profile).where(warmup_profiles: { warmup_enabled: true, last_warmup_at: nil }).count
-    @due_count = Account.joins(:warmup_profile).where(warmup_profiles: { warmup_enabled: true }).where("warmup_profiles.last_warmup_at IS NULL OR warmup_profiles.last_warmup_at < ?", 48.hours.ago).count
+    # 总计 = 各机器行之和（口径与分机器行完全一致：status [0,3] + 该机器浏览器 + warmup_enabled）
+    # 之前总计单独查询时缺 status / browser 过滤，导致总计 > 各机器行之和
+    @total_count   = @machine_stats.sum { |s| s[:total] }
+    @enabled_count = @machine_stats.sum { |s| s[:enabled] }
+    @due_count     = @machine_stats.sum { |s| s[:due] }
   end
 
   def show
