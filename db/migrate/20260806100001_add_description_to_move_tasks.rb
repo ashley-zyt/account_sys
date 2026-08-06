dcabf33304924a3eed9363fdb97c45a7e7b3014a
lijinmin <>
2026-08-06 16:02:22 +0800

    m

--- db/migrate/20260806100001_add_description_to_move_tasks.rb
+++ db/migrate/20260806100001_add_description_to_move_tasks.rb
@@ -1,4 +1,4 @@
-class AddDescriptionToMoveTasks < ActiveRecord::Migration[7.2]
+class AddDescriptionToMoveTasks < ActiveRecord::Migration[6.1]
   def change
     add_column :move_tasks, :description, :string, comment: "视频描述"
   end
