0cacc3338f5257e74b0fdfdac70da5c96ce6bca6
ashly <zhaoyuting@china-revival.com>
2026-09-08 14:03:20 +0800

    1

--- lib/kol_account_allocator.rb
+++ lib/kol_account_allocator.rb
@@ -10,8 +10,8 @@ class KolAccountAllocator
   MAX_CONTACTS_PER_DAY = 5
   MIN_AVG_VIEWS = 10
   SLEEP_HOURS = 24
-  # 当前已接通 twitter / tiktok / instagram；其它平台暂未开通
-  SUPPORTED_PLATFORMS = %w[twitter tiktok instagram].freeze
+  # 当前已接通 twitter / tiktok / instagram / facebook
+  SUPPORTED_PLATFORMS = %w[twitter tiktok instagram facebook].freeze
 
   class << self
     def supported_platform?(platform)
